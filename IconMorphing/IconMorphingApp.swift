f932fc1c5b4178a632e763206a7e55514196cb2e
elichartnett <elichartnett@icloud.com>
2023-04-11 15:44:26 -0400

    Initial Commit

---
+++ IconMorphing/IconMorphingApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconMorphingApp.swift
+//  IconMorphing
+//
+//  Created by Eli Hartnett on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IconMorphingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
